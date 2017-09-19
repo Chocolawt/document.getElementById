006e20f40fb05ce05bfd4062d9bf2d8908db7597
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-09-19 08:58:46 -1000

    Create pictureBSB.js

---
+++ pictureBSB.js
@@ -0,0 +1,8 @@
+//HTML:
+<button onclick ="styleBackground()">Click me</button>
+
+//JS:
+function styleBackground() {
+	document.body.style.background = "url('http://buzzsharer.com/wp-content/uploads/2016/10/corgi-photo.jpg')"
+	document.body.style.backgroundSize= "cover"
+}
